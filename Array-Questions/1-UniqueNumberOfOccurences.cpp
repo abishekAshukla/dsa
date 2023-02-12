f852e2045a4b16b819f1a595063eea58337e44bc
Abhishek Shukla <90856267+abhishek2002Git@users.noreply.github.com>
2023-02-12 20:44:06 +0530

    unique no of occurunces

--- Array-Questions/1-UniqueNumberOfOccurences.cpp
+++ Array-Questions/1-UniqueNumberOfOccurences.cpp
@@ -1,14 +1,67 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int UniqueNumberOfOccurences(int arr[], int n){
-    
+// https://leetcode.com/problems/unique-number-of-occurrences/submissions/896602755/
+
+void printVector(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+bool findDuplicates(vector<int> &arr)
+{
+    for (int i = 1; i < arr.size(); i++)
+    {
+        // sort(counter.begin(), counter.end());
+        if (arr[i] == arr[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool uniqueOccurrences(vector<int> &arr)
+{
+    // sort(arr.begin(), arr.end());
+    vector<int> counter;
+    int count = 1;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] == arr[i - 1])
+        {
+            count++;
+        }
+        else
+        {
+            counter.push_back(count);
+            count = 1;
+        }
+    }
+    counter.push_back(count);
+
+    for (int i = 1; i < counter.size(); i++)
+    {
+        // sort(counter.begin(), counter.end());
+        if (counter[i] == counter[i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
 {
-    int arr[] = {1,2,1,3,4,4,4,3,3,3};
-    int sizeOfArr = sizeof(arr)/sizeof(int);
+    // vector<int> vect{1, 2, 2, 1, 1, 3};
+    vector<int> vect{1, 1, 1, 2, 2, 3};
+    cout<<uniqueOccurrences(vect);
 
     return 0;
 }
